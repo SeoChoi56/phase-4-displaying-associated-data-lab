79a863abeb5f4bdf1d9568f30cc94b9cffe08bc9
Seo Choi <seochoi56@gmail.com>
2022-11-05 20:41:41 -0400

    commit tree 21f253f6e5c1efb0b21928acaaf9fb33068f4055

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        #ask tyler between find and find_by
+        render json: user, include: :items
+    end
+
 end
